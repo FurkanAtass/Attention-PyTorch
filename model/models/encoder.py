e6af5e9cf4065b0a138aebabd4b9c473d15a5db5
FurkanAtass <s.furkanatas@gmail.com>
2025-05-20 00:41:41 +0300

    encoder moder class

---
+++ model/models/encoder.py
@@ -0,0 +1,28 @@
+import torch
+from torch import nn
+from embedding.input_embedding import InputEmbedding
+from embedding.positional_encoding import PositionalEncoding
+from blocks.encoder_block import EncoderBlock
+
+class Encoder(nn.Module):
+    def __init__(self, vocab_size, d_model, num_heads, dff, num_layers, max_len, dropout=0.1):
+        super(Encoder, self).__init__()
+
+        self.input_embedding = InputEmbedding(vocab_size, d_model)
+        self.positional_encoding = PositionalEncoding(d_model, max_len, dropout)
+
+        self.encoder_blocks = nn.ModuleList([
+            EncoderBlock(d_model, num_heads, dff, dropout) for _ in range(num_layers)
+        ])
+
+    def forward(self, x):
+        # x: (batch_size, T)
+        x = self.input_embedding(x)
+        # x: (batch_size, T, d_model)
+        x = self.positional_encoding(x)
+        # x: (batch_size, T, d_model)
+        for block in self.encoder_blocks:
+            x = block(x)
+            # x: (batch_size, T, d_model)
+        return x
+
