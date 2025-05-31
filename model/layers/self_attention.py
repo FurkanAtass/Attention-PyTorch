fix: correct train_split and batch_size in config; update self-attention mask fill value; refactor training loop in train.py

model/layers/self_attention.py
@@ -13,7 +13,7 @@ class SelfAttention(nn.Module):
         attention = torch.matmul(q, k.transpose(-2, -1)) / (d ** 0.5)
 
         if mask is not None:
-            attention = attention.masked_fill(mask == 0, float('-inf')) # values 0 in mask are set to -inf
+            attention = attention.masked_fill(mask == 0, -1e9) # values 0 in mask are set to -inf
 
         attention = self.softmax(attention) # softmax to get attention weights q * k^T
 

model/models/transformer.py
@@ -66,7 +66,7 @@ class Transformer(nn.Module):
         dec_output = self.decoder(dec_input, enc_output, dec_padding_mask, enc_padding_mask)
 
         final_output = self.final_layer(dec_output)
-        final_output = self.dropout(final_output)
+        # final_output = self.dropout(final_output)
 
         return final_output
 

train.py
@@ -18,8 +18,6 @@ def prepare_model_and_data(config):
     encoder_vocab_size = source_tokenizer.get_vocab_size()
     decoder_vocab_size = target_tokenizer.get_vocab_size()
 
-    print(f"Encoder vocab size: {encoder_vocab_size}")
-    print(f"Decoder vocab size: {decoder_vocab_size}")
     model = Transformer(
         encoder_padding_idx,
         decoder_padding_idx,
@@ -42,13 +40,52 @@ config = get_config()
  loss_fn) = prepare_model_and_data(config)
 
 model.train()
-model.to("mps")
-batch = next(iter(train_dataloader))
+model.to(device)
 
-enc_input: torch.Tensor = batch["encoder_input"].to("mps")
-dec_input = batch["decoder_input"].to("mps")
+print(f"Train dataloader length: {len(train_dataloader)}")
+print(f"Validation dataloader length: {len(valid_dataloader)}")
+avg_train_loss = 0.
+avg_validation_loss = 0.
+avg_acc = 0.
 
-print(f"enc_input shape: {enc_input.device}")
-print(f"dec_input shape: {dec_input}")
-output = model(enc_input, dec_input)
-print(f"output shape: {output.shape}")
+for epoch in range(config["num_epochs"]):
+    for batch in train_dataloader:
+
+        enc_input = batch["encoder_input"].to(device)
+        dec_input = batch["decoder_input"].to(device)
+        label = batch["decoder_output"].to(device)
+
+        prediction = model(enc_input, dec_input)
+
+        optimizer.zero_grad()
+
+        # loss = loss_fn(prediction.view(-1, model.decoder_vocab_size), label.view(-1))
+        loss = loss_fn(prediction.permute(0, 2, 1), label)
+
+        loss.backward()
+
+        optimizer.step()
+
+        avg_train_loss += loss.item()
+        print(f"Train Loss: {loss.item():.4f}")
+
+    avg_train_loss /= len(train_dataloader)
+
+    for batch in valid_dataloader:
+        enc_input = batch["encoder_input"].to(device)
+        dec_input = batch["decoder_input"].to(device)
+        label = batch["decoder_output"].to(device)
+
+        with torch.inference_mode():
+            prediction = model(enc_input, dec_input)
+
+            loss = loss_fn(prediction.permute(0, 2, 1), label)
+
+            avg_validation_loss += loss.item()
+            print(f"Validation Loss: {loss.item():.4f}")
+
+    avg_validation_loss /= len(valid_dataloader)
+
+    print(f"Epoch {epoch + 1}/{config['num_epochs']}, "
+          f"Train Loss: {avg_train_loss:.4f}, "
+          f"Validation Loss: {avg_validation_loss:.4f}")