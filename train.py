a88ad1da1de23505c0f7c98148be21db05389bba
FurkanAtass <s.furkanatas@gmail.com>
2025-05-31 16:24:25 +0300

    feat: enhance training loop with accuracy tracking and detailed logging; add pendulum for time management

--- train.py
+++ train.py
@@ -5,7 +5,9 @@ from data_utils.tokenizer import get_dataset
 
 from config import get_config
 
+import pendulum
 device = "cuda" if torch.cuda.is_available() else ("mps" if torch.backends.mps.is_available() else "cpu")
+PRINT_EVERY_N_BATCHES = 100
 
 def prepare_model_and_data(config):
     (train_dataloader, 
@@ -39,17 +41,27 @@ config = get_config()
  optimizer,
  loss_fn) = prepare_model_and_data(config)
 
-model.train()
 model.to(device)
-
+torch.save(model.state_dict(), "model.pth")
 print(f"Train dataloader length: {len(train_dataloader)}")
 print(f"Validation dataloader length: {len(valid_dataloader)}")
-avg_train_loss = 0.
-avg_validation_loss = 0.
-avg_acc = 0.
 
+
+def accuracy(predictions, labels):
+    _, prediction_classes = torch.max(predictions, dim=2)
+    correct = (prediction_classes == labels).float()
+    acc = correct.sum() / correct.numel()
+    return acc.item()
+  
+# save loss acc and model
 for epoch in range(config["num_epochs"]):
-    for batch in train_dataloader:
+    avg_train_loss = 0.
+    avg_validation_loss = 0.
+    avg_acc = 0.
+    epoch_start_time = pendulum.now()
+
+    model.train()
+    for i, batch in enumerate(train_dataloader):
 
         enc_input = batch["encoder_input"].to(device)
         dec_input = batch["decoder_input"].to(device)
@@ -67,11 +79,18 @@ for epoch in range(config["num_epochs"]):
         optimizer.step()
 
         avg_train_loss += loss.item()
-        print(f"Train Loss: {loss.item():.4f}")
 
+        if (i + 1) % PRINT_EVERY_N_BATCHES == 0:
+            print(f"Epoch {epoch + 1}/{config['num_epochs']} | "
+                  f"Batch {i + 1}/{len(train_dataloader)} | "
+                  f"Train Loss: {loss.item():.4f} | "
+                  f"Avg Train Loss: {avg_train_loss / (i + 1):.4f}")
+        # if i == 100:
+        #     break 
     avg_train_loss /= len(train_dataloader)
 
-    for batch in valid_dataloader:
+    model.eval()
+    for i, batch in enumerate(valid_dataloader):
         enc_input = batch["encoder_input"].to(device)
         dec_input = batch["decoder_input"].to(device)
         label = batch["decoder_output"].to(device)
@@ -82,10 +101,20 @@ for epoch in range(config["num_epochs"]):
             loss = loss_fn(prediction.permute(0, 2, 1), label)
 
             avg_validation_loss += loss.item()
-            print(f"Validation Loss: {loss.item():.4f}")
+
+            batch_acc = accuracy(prediction, label)
+            avg_acc += batch_acc
+
+            if (i + 1) % PRINT_EVERY_N_BATCHES == 0:
+                print(f"Validation Batch {i + 1}/{len(valid_dataloader)} | "
+                      f"Loss: {loss.item():.4f} | "
+                      f"Accuracy: {batch_acc:.4f}")
 
     avg_validation_loss /= len(valid_dataloader)
-
-    print(f"Epoch {epoch + 1}/{config['num_epochs']}, "
-          f"Train Loss: {avg_train_loss:.4f}, "
-          f"Validation Loss: {avg_validation_loss:.4f}")
+    avg_acc /= len(valid_dataloader)
+    epoch_end_time = pendulum.now()
+    print(f"Epoch {epoch + 1}/{config['num_epochs']} | "
+          f"Train Loss: {avg_train_loss:.4f} | "
+          f"Validation Loss: {avg_validation_loss:.4f} | "
+          f"Validation Accuracy: {avg_acc:.4f} | "
+          f"Time: {(epoch_end_time - epoch_start_time).in_words()} ")
